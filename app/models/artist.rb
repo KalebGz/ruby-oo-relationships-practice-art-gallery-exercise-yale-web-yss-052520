e3ae4a6290b06130fd2fd615a08325173d31d7c8
KalebGz <kaleb.zeray@gmail.com>
2020-06-14 00:04:43 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,57 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+
+    @@all << self
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+
+  def paintings
+    Painting.all.select{|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map(&:gallery)
+  end
+
+  def cities
+    galleries.map(&:city)
+  end
+
+  def self.total_experience
+    Artist.all.sum(&:years_experience)
+  end
+
+  def self.most_prolific
+    Artist.all.max{|artist| artist.paintings.count / artist.years_experience}
+  end
+
+  def self.all
+    @@all
   end
 
 end
+
+# ARTIST
+# Artist.all
+# Returns an array of all the artists
+# Artist#paintings
+# Returns an array all the paintings by an artist
+# Artist#galleries
+# Returns an array of all the galleries that an artist has paintings in
+# Artist#cities
+# Return an array of all cities that an artist has paintings in
+# Artist.total_experience
+# Returns an integer that is the total years of experience of all artists
+# Artist.most_prolific
+# Returns an instance of the artist with the highest amount of paintings per year of experience.
+# Artist#create_painting
+# Given the arguments of title, price and gallery, creates a new painting belonging to that artist
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,15 @@
 require_relative '../config/environment.rb'
 
 
+joe = Artist.new("joe", 15)
+jack = Artist.new("jack", 12)
 
+g1 = Gallery.new("Smithisonian", "DC")
+g2 = Gallery.new("British Art Gallery", "CT")
+
+p1 = Painting.new("Stary Night", 2000, joe, g1)
+p2 = Painting.new("BIG CAT", 900, joe, g2)
+p3 = Painting.new("Vase", 3, jack, g1)
 
 binding.pry
 
